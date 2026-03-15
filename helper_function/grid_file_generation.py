9c394bbf9f5429cd3762cb385b54a044c13ec097
Zhenzhong Xing <zx296@mehta-cpu-01.ece.cornell.edu>
2026-03-14 23:13:53 -0400

    finish loop

--- helper_function/grid_file_generation.py
+++ helper_function/grid_file_generation.py
@@ -2,7 +2,7 @@ import numpy as np
 import nses
 
 
-def generate_grid_file(x_scan=None, y_scan=None, z_scan=None, filename="z_scan.h5"):
+def generate_grid_file(x_scan=None, y_scan=None, z_scan=None, filename="z_scan.h5", gridID=0):
 	if x_scan is None:
 		x_scan = [0]
 	if y_scan is None:
@@ -14,7 +14,7 @@ def generate_grid_file(x_scan=None, y_scan=None, z_scan=None, filename="z_scan.h
 	y_grid = np.array(y_scan) * 1e-6
 	z_grid = np.array(z_scan) * 1e-6
 
-	nses.writeGrid(filename, 0, "3DRECTMESH", x_grid, y_grid, z_grid, True)
+	nses.writeGrid(filename, gridID, "3DRECTMESH", x_grid, y_grid, z_grid, True)
 	print(filename)
 	return filename
 
