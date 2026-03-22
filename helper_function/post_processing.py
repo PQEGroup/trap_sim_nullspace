7f8a0ea97753707ee8ac6741755194632e0564b5
Zhenzhong Xing <zx296@mehta-cpu-01.ece.cornell.edu>
2026-03-22 12:42:49 -0400

    transport sim

--- helper_function/post_processing.py
+++ helper_function/post_processing.py
@@ -5,6 +5,9 @@ def post_process(result_filename="rf_sim.in.h5", output_filename="rf_sim_out.h5"
     electrodes = nses.getSimulationInfo(result_filename, "electrodes")
     x, y, z = nses.getSimulationInfo(result_filename, "grid", gridID)
 
+    electrodes = [22, 23, 24, 25, 27]
+    print(electrodes)
+
     if verbose:
         print(len(electrodes))
         print(x[:, 0, 0])
@@ -18,6 +21,7 @@ def post_process(result_filename="rf_sim.in.h5", output_filename="rf_sim_out.h5"
         for el in electrodes:
             phi = nses.getPotentials(result_filename, gridID, el)
             f.create_dataset(str(el), data=phi)
+            
 
     print(output_filename)
     return output_filename
