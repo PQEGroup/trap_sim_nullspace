ed5d49423d902c399867dc8bf77823dee3e91364
Technium7 <108077005+Technium7@users.noreply.github.com>
2026-04-06 15:42:19 -0400

    voltage saved in artiq style

--- helper_function/post_process.py
+++ helper_function/post_process.py
@@ -1,6 +1,7 @@
 def post_process(result_filename="rf_sim.in.h5", output_filename="rf_sim_out.h5", gridID=0, verbose=True):
     import nses
     import h5py
+    
 
     electrodes = nses.getSimulationInfo(result_filename, "electrodes")
     x, y, z = nses.getSimulationInfo(result_filename, "grid", gridID)
@@ -69,6 +70,23 @@ def read_post_processed(filename="rf_sim_out.h5", electrode_list=None):
 
     return x, y, z, potentials
 
+def save_to_artiq(Vks, electrode, mapping_dict, filename="artiq_output.csv"):
+
+    import numpy as np
+
+    input_num = mapping_dict.get("artiq_input_num")
+    if Vks.ndim == 1:
+        output_num = np.zeros((1,input_num), dtype=np.float64)
+    elif Vks.ndim == 2:
+        output_num = np.zeros((Vks.shape[0], input_num), dtype=np.float64)
+
+    for el, voltage in zip(electrode, Vks):
+        #print(el, voltage)
+        output_num[:, mapping_dict[el]-1] = voltage
+
+    with open(filename, "w") as f:
+        for i in range(output_num.shape[0]):
+            f.write(", ".join([f"{output_num[i, j]:.6f}" for j in range(output_num.shape[1])]) + "\n")
 
 if __name__ == "__main__":
     post_process()
